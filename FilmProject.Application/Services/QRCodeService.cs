fefa01439e4d773f3c5ddaaabecb7b51615a0c41
UtkuAraal <utkuaraal73@gmail.com>
2023-04-17 15:36:59 +0300

    Email services and qr services added

---
+++ FilmProject.Application/Services/QRCodeService.cs
@@ -0,0 +1,27 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmProject.Application.Services
+{
+    public class QRCodeService
+    {
+        private readonly QRCodeGenerator _generator;
+
+        public QRCodeService(QRCodeGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public string GetQRCodeAsBase64(string textToEncode)
+        {
+            QRCodeData qrCodeData = _generator.CreateQrCode(textToEncode, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+
+            return Convert.ToBase64String(qrCode.GetGraphic(4));
+        }
+    }
+}
