3b88fef6e4ef0a07d668c4472b667af485b39986
UtkuAraal <utkuaraal73@gmail.com>
2023-04-19 17:28:17 +0300

    bug fixed

--- FilmProject.Infrastructure/Repository/Concrete/FavoriteRepository.cs
+++ FilmProject.Infrastructure/Repository/Concrete/FavoriteRepository.cs
@@ -1,4 +1,6 @@
-﻿using FilmProject.Infrastructure.Repository.Abstract;
+﻿using FilmProject.Domain.Entities;
+using FilmProject.Infrastructure.Data;
+using FilmProject.Infrastructure.Repository.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace FilmProject.Infrastructure.Repository.Concrete
 {
-    internal class FavoriteRepository : IFavoriteRepository
+    public class FavoriteRepository : IFavoriteRepository
     {
+        private readonly ApplicationDbContext _context;
+        public FavoriteRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task AddFavoriteMovie(Favorite favorite)
+        {
+            _context.Add(favorite);
+            await _context.SaveChangesAsync();
+        }
     }
 }
