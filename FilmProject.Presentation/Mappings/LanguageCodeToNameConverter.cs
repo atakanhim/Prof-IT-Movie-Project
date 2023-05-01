b988a620382117145be6fbcd0eb1e6ce5e3e4f6a
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2023-05-01 20:27:13 +0300

    language dönüşümü mapping işlemi ile yapıldı

---
+++ FilmProject.Presentation/Mappings/LanguageCodeToNameConverter.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace FilmProject.Presentation.Mappings
+{
+    public class LanguageCodeToNameConverter : IValueConverter<string, string>
+    {
+        public string Convert(string source, ResolutionContext context)
+        {
+            switch (source)
+            {
+                case "tr-TR":
+                    return "Türkçe";
+                case "en-US":
+                    return "İngilizce";
+                default:
+                    return source;
+            }
+        }
+    }
+}
