97657fd911f8f7ecc026a9497853b8325bcf4751
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2023-05-02 00:01:59 +0300

    dil sayıları attrıldı

--- FilmProject.Presentation/Mappings/LanguageCodeToNameConverter.cs
+++ FilmProject.Presentation/Mappings/LanguageCodeToNameConverter.cs
@@ -12,6 +12,26 @@ namespace FilmProject.Presentation.Mappings
                     return "Türkçe";
                 case "en-US":
                     return "İngilizce";
+                case "fr-FR":
+                    return "Fransızca";
+                case "es-ES":
+                    return "İspanyolca";
+                case "de-DE":
+                    return "Almanca";
+                case "it-IT":
+                    return "İtalyanca";
+                case "pt-PT":
+                    return "Portekizce";
+                case "ar-SA":
+                    return "Arapça";
+                case "zh-CN":
+                    return "Çince";
+                case "ja-JP":
+                    return "Japonca";
+                case "ko-KR":
+                    return "Korece";
+                case "ru-RU":
+                    return "Rusça";
                 default:
                     return source;
             }
