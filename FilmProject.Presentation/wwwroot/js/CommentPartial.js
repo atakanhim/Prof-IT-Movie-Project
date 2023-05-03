77ae3370ead1d9111db2487501a10dda3dfcdf46
Boz-Yusuf <boz35yusuf@gmail.com>
2023-05-03 16:53:14 +0300

    Comments like refleshed with page

--- FilmProject.Presentation/wwwroot/js/CommentPartial.js
+++ FilmProject.Presentation/wwwroot/js/CommentPartial.js
@@ -1,7 +1,7 @@
 ﻿$(function(){
     $(".like-button").click(function () {
         let commentIdValue = $(this).parent().data("itemid");
-
+        refreshCommentLike();
 
         var commentLikeModel = {
             CommentId: commentIdValue,
@@ -16,35 +16,41 @@
             data: model,
             success: function (response) {
                 console.log("Listeme ekleme başarılı");
+                refreshCommentLike();
             },
             error: function (xhr, status, error) {
                 alertify.error(xhr.responseText);
             }
         });
 
-
+      
 
   
     })
+    refreshCommentLike();
 
-    
+    function refreshCommentLike() {
+        let comments = $(".comment-like-item")
+        comments.each(function () {
+            let commentLikeSection = $(this);
+            let commentLikeId = ($(this).data("itemid"));
+            $.get('/CommentLike/IsCommentLiked', { commentId: commentLikeId }, function (data, textStatus, jqXHR) {
+                let commentLikeStatue = data;
+                console.log(commentLikeId + "   " +data)
+                if (commentLikeStatue == true) {
+                    commentLikeSection.find("i").addClass("like-button--active");
+                } else {
+                    console.log("HEYYO")
+                    commentLikeSection.find("i").removeClass("like-button--active");
+                }
+                //console.log(commentLikeStatue);
+            });
 
-    let comments = $(".comment-like-item")
-    comments.each(function () {
-        let commentLikeSection = $(this);
-        let commentLikeId = ($(this).data("itemid"));
-        $.get('/CommentLike/IsCommentLiked', { commentId: commentLikeId }, function (data, textStatus, jqXHR) {
-            let commentLikeStatue = data;
-            if (commentLikeStatue == true) {
-                commentLikeSection.find("i").addClass("like-button--active");
-            } else {
-                commentLikeSection.find("i").removeClass("like-button--active");
-            }
-            console.log(commentLikeStatue);
-        });
+            console.log($(this))
+        })
+    }
 
-        console.log($(this))
-    })
+
 
 
 
