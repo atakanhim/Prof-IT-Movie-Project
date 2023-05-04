1fcf5833785af2257eeb48541a2051850bb7cee5
Boz-Yusuf <boz35yusuf@gmail.com>
2023-05-04 22:12:47 +0300

    Kategory seçince otomatik ekranın spinner altına kayması

--- FilmProject.Presentation/wwwroot/js/anasayfa.js
+++ FilmProject.Presentation/wwwroot/js/anasayfa.js
@@ -11,7 +11,16 @@ $(document).ready(function () {
         $("#movieRenderTitle").html("Tavsiye Filmler");
         $("#movieRender").html(data);
 
-
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            var target = this.hash;
+            var $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 1000, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
 
     });
 
